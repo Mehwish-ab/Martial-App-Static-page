9e34b17d2d56f48d8f6877bc190540f3bbc16313
umii141 <umi039000@gmail.com>
2023-12-04 12:01:35 +0500

    School Payment Page Pixel Perfect

--- src/screens/CreateSchool/AddPaymentSchool/styles.ts
+++ src/screens/CreateSchool/AddPaymentSchool/styles.ts
@@ -98,12 +98,13 @@ export const AddPaymentMethod = styled.div`
   .ant-table-thead > tr > th{
     color: ${darkGery};
     font-family: ${fontFamilyMedium};
-    font-size: 16px !important;
+    font-size: 16px;
     font-weight: 500;
   }
   .ant-table-thead > tr > td{
     color: ${darkGery};
-    font-family: ${fontFamilyRegular};    font-size: 14px !important;
+    font-family: ${fontFamilyRegular};    
+    font-size: 14px !important;
     line-height: 23px;
     font-weight: 400;
   }
@@ -111,10 +112,11 @@ export const AddPaymentMethod = styled.div`
   .ant-table-tbody > tr > td{
     width: 19%;
     font-size: 16px;
+    font-family: ${fontFamilyMedium};
     font-style: normal;
     font-weight: 400;
     line-height: 22px; 
-    padding: 16px 20px;
+    padding: 11px 20px;
   }
   .ant-table-thead > tr > th:last-child, 
   .ant-table-tbody > tr > td:last-child{

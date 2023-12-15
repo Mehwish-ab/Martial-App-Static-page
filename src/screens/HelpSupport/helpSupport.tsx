d1949200f2b39e9b784ce9f4b91c7d802560f16d
umii141 <umi039000@gmail.com>
2023-12-15 11:49:27 +0500

    help & supprot

---
+++ src/screens/HelpSupport/helpSupport.tsx
@@ -0,0 +1,295 @@
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Form } from "antd";
+import FormControl from "../../components/FormControl";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import {
+    fontFamilyMedium,
+    fontFamilyRegular,
+    lightBlue3,
+    pureDark2,
+} from "../../components/GlobalStyle";
+import Head from "../../components/Head/Head";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import EnnvisionModal from "../../components/CustomModals/EnnvisionModal";
+import CustomModal from "../../components/Modal/CustomModal";
+import { useAppSelector } from "../../app/hooks";
+import { validationFinder } from "../../utils/utilities";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { signup_url } from "../../utils/api_urls";
+
+import "react-phone-number-input/style.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import HelpSupportStyling from "./style";
+
+// create user initial values types
+type initialValuesType = {
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    phoneNumber: string;
+    password: string;
+    confirmPassword?: string;
+};
+
+const HelpSupport = () => {
+    const [isShowModal, setIsShowModal] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [terms] = useState(false);
+
+    const scrollViewRef = useRef<any>();
+    const navigate = useNavigate();
+    const { selectedLanguage } = useSelector(
+        (state: RootState) => state.selectedLanguage
+    );
+    const initialValues: initialValuesType = {
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        phoneNumber: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const { result: userLocation } = useAppSelector(
+        (state) => state.userLocation
+    );
+    // get Validations from redux appData
+    const {
+        countryName: {
+            results: { countryCode, name },
+        },
+    } = useAppSelector((state) => state.appData.data);
+    // create user initial values
+    useEffect(() => {
+        const countrySelect = document.querySelector(
+            ".PhoneInput .PhoneInputCountry"
+        );
+        const phoneNumberInput = document.querySelector(".PhoneInput input");
+
+        if (countrySelect) {
+            if (selectedLanguage === "ur" || selectedLanguage === "ar") {
+                countrySelect.classList.remove("country-left-to-right-border-radius");
+                countrySelect.classList.add("country-right-to-left-border-radius");
+            } else {
+                countrySelect.classList.add("country-left-to-right-border-radius");
+                countrySelect.classList.remove("country-right-to-left-border-radius");
+            }
+        }
+        if (phoneNumberInput) {
+            if (selectedLanguage === "ur" || selectedLanguage === "ar") {
+                phoneNumberInput.classList.add(
+                    "phone-number-right-to-left-border-radius"
+                );
+                phoneNumberInput.classList.remove(
+                    "phone-number-left-to-right-border-radius"
+                );
+            } else {
+                phoneNumberInput.classList.add(
+                    "phone-number-left-to-right-border-radius"
+                );
+                phoneNumberInput.classList.remove(
+                    "phone-number-right-to-left-border-radius"
+                );
+            }
+        }
+    }, [selectedLanguage]);
+    // user validations
+    const firstName = validationFinder("USER_FIRSTNAME")!;
+    const lastName = validationFinder("USER_LASTNAME")!;
+    const emailAddress = validationFinder("EMAIL_ADDRESS")!;
+    const phoneNumber = validationFinder("PHONE_NUMBER")!;
+    const password = validationFinder("PASSWORD")!;
+    // user regExpressions
+    const firstNameReg = new RegExp(firstName.pattern);
+    const lastNameReg = new RegExp(lastName.pattern);
+    const emailAddressReg = new RegExp(emailAddress.pattern);
+    const phoneNumberReg = new RegExp(phoneNumber.pattern);
+    const passwordReg = new RegExp(password.pattern);
+
+    // create user validation schema
+    const validationSchema = Yup.object({
+        firstName: Yup.string()
+            .required(firstName.notBlankMsgEn)
+            .matches(firstNameReg, firstName.patternMsgEn),
+        lastName: Yup.string()
+            .required(lastName.notBlankMsgEn)
+            .matches(lastNameReg, lastName.patternMsgEn),
+        emailAddress: Yup.string()
+            .required(emailAddress.notBlankMsgEn)
+            .matches(emailAddressReg, emailAddress.patternMsgEn),
+        phoneNumber: Yup.string()
+            .required(phoneNumber.notBlankMsgEn)
+            .matches(phoneNumberReg, phoneNumber.patternMsgEn),
+        password: Yup.string()
+            .required(password.notBlankMsgEn)
+            .matches(passwordReg, password.patternMsgEn),
+        confirmPassword: Yup.string()
+            .required("confirm password is required!")
+            .oneOf([Yup.ref("password")], "passwords must match"),
+    });
+    // create user data submit
+    const onSubmit = async (values: initialValuesType) => {
+        if (!terms) {
+            setShowTermsError(true);
+            setTimeout(() => {
+                setShowTermsError(false);
+            }, 2000);
+            if (scrollViewRef.current) {
+                scrollViewRef.current.scrollToEnd({ animated: true });
+            }
+            return;
+        }
+        // get all values other than confirm password
+        const allValues = {
+            ...values,
+        };
+        delete allValues.confirmPassword;
+        const registerUserValues = {
+            ...allValues,
+            phoneNumber: values.phoneNumber.toString(),
+            countryName: name,
+            countryCode: countryCode,
+        };
+        const userData = {
+            ...registerUserValues,
+            roleId: 1,
+            channel: "Web",
+            address: userLocation?.address,
+            city: userLocation?.city,
+            state: userLocation?.state,
+        };
+        try {
+            setIsLoading(true);
+            await axios.post(signup_url, userData);
+            setIsShowModal(true);
+            setTimeout(() => {
+                setIsShowModal(false);
+                navigate("/login");
+            }, 2000);
+            setIsLoading(false);
+        } catch (error: any) {
+            setIsLoading(false);
+            toast(error.response.data.responseMessage, {
+                type: "error",
+                autoClose: 1000,
+            });
+        }
+    };
+
+    return (
+        <>
+            <Head title="Help & Support" />
+            <CustomModal
+                isModalVisible={isShowModal}
+                setIsModalVisible={setIsShowModal}
+                showCloseBtn={false}
+            >
+                <EnnvisionModal
+                    doTask={() => {
+                        navigate("/login");
+                        setIsShowModal(false);
+                    }}
+                    title="Account Created Successfully!"
+                    description="Thank You For Joining Us And We're Excited To Have You On Board"
+                />
+            </CustomModal>
+            <HelpSupportStyling>
+                <div className="inner-container-card">
+                    <div className="inner-container-card-inner">
+                        <h6 className="title mb-0 text-center">
+                            Help & Support
+                        </h6>
+                        <p className="text-center message mt-10 mb-0">
+                            Leave us your info and we will get back to you.
+                        </p>
+                        <div className="inner-container-card-form">
+                            <Formik
+                                initialValues={initialValues}
+                                validationSchema={validationSchema}
+                                onSubmit={onSubmit}
+                            >
+                                {(formik) => {
+                                    return (
+                                        <Form
+                                            name="basic"
+                                            onFinish={formik.handleSubmit}
+                                            autoComplete="off"
+                                        >
+                                            <div className="register-input-fields">
+                                                <div className="mt-20">
+                                                    <FormControl
+                                                        control="input"
+                                                        type="text"
+                                                        name="firstName"
+                                                        label="Full Name"
+                                                        fontSize="16px"
+                                                        border="none"
+                                                        placeholder="Full Name"
+                                                        labelFamily={fontFamilyMedium}
+                                                        className="customInput"
+                                                    />
+                                                </div>
+                                                <div className="mt-20">
+                                                    <FormControl
+                                                        control="input"
+                                                        type="text"
+                                                        name="lastName"
+                                                        fontSize="16px"
+                                                        label="Email"
+                                                        border="none"
+                                                        labelFamily={fontFamilyMedium}
+                                                        placeholder="abc@gmail.com"
+                                                        className="customInput"
+                                                    />
+                                                </div>
+                                                <div className="mt-20">
+                                                    <FormControl
+                                                        control="textarea"
+                                                        type="text"
+                                                        name="description"
+                                                        fontFamily={fontFamilyRegular}
+                                                        // prefix={<img src={lock_icon} alt="lock_icon" />}
+                                                        label="Message"
+                                                        padding="10px"
+                                                        placeholder="Write a message"
+                                                        className="customInput"
+                                                        height="200px"
+                                                    />
+                                                </div>
+                                                <div className="mt-20">
+                                                    <CustomButton
+                                                        bgcolor={lightBlue3}
+                                                        fontFamily={`${fontFamilyMedium}`}
+                                                        textTransform="Captilize"
+                                                        color={pureDark2}
+                                                        padding="12.5px 8px"
+                                                        width="100%"
+                                                        type="submit"
+                                                        title="Save"
+                                                        fontSize="16px"
+                                                        border=""
+                                                        loading={isLoading}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </Form>
+                                    );
+                                }}
+                            </Formik>
+                        </div>
+                    </div>
+                </div>
+            </HelpSupportStyling>
+        </>
+    );
+};
+
+export default HelpSupport;
+function setShowTermsError(arg0: boolean) {
+    throw new Error("Function not implemented.");
+}
+
---
+++ src/screens/HelpSupport/style.ts
@@ -0,0 +1,241 @@
+import styled from "styled-components";
+import {
+  darkBlue,
+  fontFamilyMedium,
+  fontFamilyRegular,
+  lightDark2,
+  lightDark3,
+  pureDark2,
+  secondaryDark3,
+  tertiaryGrey12,
+  tertiaryGrey24,
+  tertiaryGrey8,
+} from "../../components/GlobalStyle";
+
+const HelpSupportStyling = styled.div`
+  background: ${tertiaryGrey8};
+  overflow-y: auto;
+  
+  .signup-text {
+    display: flex;
+    justify-content:center;
+    align-items:center;
+
+    p,a,span {
+        margin-bottom: 0
+        font-family: ${fontFamilyRegular};
+        color: ${pureDark2};
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+      a{
+        font-family: ${fontFamilyMedium};
+        text-decoration: underline;
+        color: ${darkBlue};
+        margin: 0 2px;
+      }
+      span{
+        font-family: ${fontFamilyMedium};
+        color: ${darkBlue};
+        a:hover{
+          text-decoration: none;
+        }
+      }
+    }
+
+    .register-input-fields {
+    label{
+      font-style: normal;
+      line-height: normal;
+      font-size: 16px;
+      color: ${pureDark2};
+      font-family: ${fontFamilyMedium};
+      margin-bottom: 8px;
+    }
+    .ant-checkbox-wrapper{
+      margin-bottom: 0px;
+    }
+  }
+  .remeberText{
+    color: ${pureDark2};
+    font-family: ${fontFamilyRegular};
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+
+  .role-section {
+    font-family: ${fontFamilyMedium};
+    .roles {
+      border: 1px solid ${tertiaryGrey24};
+    }
+  }
+  .inner-container {
+    padding-top: 8px;
+    &-card {
+      &-inner{
+        width: 100%;
+      }
+      .title {
+        font-family: ${fontFamilyMedium};
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+        color: ${pureDark2};
+      }
+      .message {
+        font-family: ${fontFamilyRegular};
+        color: ${lightDark3};
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+      max-width: 485px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: white;
+      border-radius: 20px;
+      margin: auto;
+      padding: 28px 23px 24px;
+      &-form {
+        width: 100%;
+      }
+    }
+  } 
+  .PhoneInput {
+    width: 100%;
+    height: 52px;
+  input{
+    height: 52px;
+  }
+  .PhoneInputCountry{
+    height: 52px;
+   }
+   }
+
+  .is-invalid {
+    height: 52px;
+    border: 1px solid #E43535;
+    background: white;
+    padding: 15.5px;
+    width: 100%;
+    width: -moz-available;
+    outline: none;
+    line-height: normal;
+    margin-bottom: 2px;
+    font-size: 16px;
+    input{
+      color: ${lightDark2};
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      font-family: ${fontFamilyRegular};
+
+    }
+  }
+  .customInput{
+    height: 52px;
+    font-size: 16px;
+    background: white;
+    padding: 16.5px;
+    width: 100%;
+    width: -moz-available;
+    outline: none;
+    line-height: normal;
+    border-radius: 8px;
+    font-family: ${fontFamilyRegular};
+    border: 1px solid ${tertiaryGrey24};
+    ::placeholder{
+      font-style: normal;
+      font-family: ${fontFamilyRegular};
+      color: ${lightDark2};
+      font-weight: 400;
+      line-height: normal;
+      font-size: 16px;
+    }
+  }
+
+  
+
+  input[type="email"]:hover {
+    border-color: #40a9ff;
+    border-right-width: 1px;
+    z-index: 1;
+  }
+  input[type="text"]:hover {
+    border-color: #40a9ff;
+    border-right-width: 1px;
+    z-index: 1;
+  }
+
+  input[type="text"] + div,
+  input[type="email"] + div, 
+  .ant-input-affix-wrapper + div{
+    text-align: right !important;
+  }
+
+  @media screen and (max-width: 425px) {
+    .inner-container {
+      &-card {
+        padding: 7px 14px;
+        &-logo {
+          h1 {
+            display: none;
+          }
+        }
+      }
+    }
+    a,p{
+      font-size:14px
+    }
+  }
+ 
+
+`;
+
+export default HelpSupportStyling;
+
+export const CreatedUserModalStyle = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .inner-container {
+    max-width: 485px;
+    background: #ffffff;
+    box-shadow: 0px 0px 16px #00000017;
+    border-radius: 7px;
+    padding: 18px;
+    img {
+      height: 60px;
+      margin-bottom: 12px;
+      width: 190px;
+    }
+    .account-created {
+      font-family: "EnnVisionsMedium";
+      font-size: 19px;
+    }
+    .message {
+      margin-top: 14px;
+      font-size: 16px;
+      width: 90%;
+      margin: 0 auto;
+      color: ${secondaryDark3};
+      padding-bottom: 10px;
+      border-bottom: 1px solid ${tertiaryGrey12};
+    }
+  }
+
+  
+`;

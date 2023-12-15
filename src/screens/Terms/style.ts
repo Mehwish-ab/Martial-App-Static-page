83aafbc5e78e6b84fa9a932b9f0bc284d2b8adbb
umii141 <umi039000@gmail.com>
2023-12-15 11:48:59 +0500

    terms page

---
+++ src/screens/Terms/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { fontFamilyMedium, fontFamilyRegular, pureDark2, tertiaryGrey24 } from "../../components/GlobalStyle";
+
+export const TermsStyling = styled.div`
+    border: 1px solid ${tertiaryGrey24};
+    backgrond-color: #fff;
+    border-radius: 20px;
+    width: 90%;
+    margin: 20px auto;
+    padding: 20px 30px;
+
+    h3{
+        font-family: ${fontFamilyMedium};
+        color: ${pureDark2};
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+    }
+    p{
+        font-family: ${fontFamilyRegular};
+        color: ${pureDark2};
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 22px; 
+    }
+
+    .mt-30{
+        margin-top: 30px;
+    }
+    
+    span{
+        font-family: ${fontFamilyMedium};
+        font-weight: 500;
+    }
+
+`;

f9a81fc58e4fe232bd7b083942a0a7c903dabb42
Umar Farooq <umaaarfarooq0@gmail.com>
2024-01-24 12:57:14 +0500

    School view issue fix

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,17 @@
 import ViewSchool from '../CreateSchool/ViewSchool/ViewSchool'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
+import CreateSchool from '../CreateSchool/CreateSchool'
 // import Profile from './Profile/Profile'
 const Home = (): JSX.Element => {
-    return <ViewSchool />
+    const { schoolData } = useSelector(
+        (state: RootState) => state.dashboardData
+    )
+    const localStorageData = localStorage.getItem('ennvision-admin:token')
+    const loginData = JSON.parse(localStorageData as any)
+    if (schoolData.schoolId <= 0) {
+        //navigate('/school/create')
+        return <CreateSchool />
+    } else return <ViewSchool />
 }
-
 export default Home
